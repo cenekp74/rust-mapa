b86a6b835dbe3df44472b1ffe1c63d86be436b17
Čeněk Použník <potuznikcenek@gmail.com>
2024-04-25 21:36:11 +0200

    added leaflet

---
+++ src/main.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([50, 14.5], 5);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
