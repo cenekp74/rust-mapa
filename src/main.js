ecb09e31935202a4bd7e5889c4dce0a08501cbe0
Čeněk Použník <potuznikcenek@gmail.com>
2024-04-26 19:28:04 +0200

    functionality for adding zoom-{zoom} classes to icons on zoom change

--- src/main.js
+++ src/main.js
@@ -6,20 +6,27 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-function showPoints(datetime, data) {
+window.datetime = '2023080112'
+
+get_data('C:/Users/potuz/Desktop/UFA/kaca/rust-mapa/data/data.csv').then(function(response) {
+    window.data = response
+    showPoints(window.datetime, window.data)
+})
+
+function showPoints(datetime, data, zoom='1') {
     let icons = [];
     data.data[datetime].forEach(value => {
-        icons.push(L.divIcon({className: 'number-icon', html: "<b>" + parseFloat(value).toFixed(2) + "</b>"}));
+        icons.push(L.divIcon({className: 'number-icon zoom-'+zoom, html: "<b>" + parseFloat(value).toFixed(2) + "</b>"}));
     })
     for (let i = 0; i < icons.length; i++) {
         L.marker([data.locations[i][1], data.locations[i][0]], {icon: icons[i]}).addTo(map)
     }
 }
 
-get_data('C:/Users/potuz/Desktop/UFA/kaca/rust-mapa/data/data.csv').then(function(data) {
-    showPoints('2023080112', data)
-})
-
+map.on('zoomend', function() {   
+    var currentZoom = map.getZoom();
+    showPoints(window.datetime, window.data, zoom=currentZoom)
+});
 
 async function get_data(filename) {
     response = await invoke('get_data', {filename:filename})
