792155bf8635d58b37ad4df998c452ac5288b537
Čeněk Použník <potuznikcenek@gmail.com>
2024-04-26 18:52:50 +0200

    added js function for showing datapoints

--- src/main.js
+++ src/main.js
@@ -6,12 +6,25 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var numberIcon = L.divIcon({className: 'number-icon', html: "<b>" + 5 + "</b>"});
-L.marker([50, 14], {icon: numberIcon}).addTo(map);
+function showPoints(datetime, data) {
+    let icons = [];
+    data.data[datetime].forEach(value => {
+        icons.push(L.divIcon({className: 'number-icon', html: "<b>" + parseFloat(value).toFixed(2) + "</b>"}));
+    })
+    for (let i = 0; i < icons.length; i++) {
+        L.marker([data.locations[i][1], data.locations[i][0]], {icon: icons[i]}).addTo(map)
+    }
+}
+
+get_data('C:/Users/potuz/Desktop/UFA/kaca/rust-mapa/data/data.csv').then(function(data) {
+    showPoints('2023080112', data)
+})
+
 
 async function get_data(filename) {
-    await invoke('get_data', {filename:filename})
+    response = await invoke('get_data', {filename:filename})
         .then((response) => {
-            console.log(response)
-    })
+            return response;
+        })
+    return JSON.parse(response)
 }
