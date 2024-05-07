85c42926724f8f00a00b924d477f23442d023fd2
Čeněk Použník <potuznikcenek@gmail.com>
2024-05-07 15:01:52 +0200

    funkce pro hledani front

--- analyza_fronty.py
+++ analyza_fronty.py
@@ -4,6 +4,7 @@
 # WIP
 
 import polars as pl
+from icecream import ic
 
 DATETIME_COLUMN_NAME = 'Datum_[rrrrmmddHH]'
 POINTS = [(10.0, 47.0), (10.0, 48.5), (10.0, 50.0), (10.0, 51.5), (10.0, 53.0), (11.5, 47.0), (11.5, 48.5), (11.5, 50.0), (11.5, 51.5), (11.5, 53.0), (13.0, 47.0), (13.0, 48.5), (13.0, 50.0), (13.0, 51.5), (13.0, 53.0), (14.5, 47.0), (14.5, 48.5), (14.5, 50.0), (14.5, 51.5), (14.5, 53.0), (16.0, 47.0), (16.0, 48.5), (16.0, 50.0), (16.0, 51.5), (16.0, 53.0), (17.5, 47.0), (17.5, 48.5), (17.5, 50.0), (17.5, 51.5), (17.5, 53.0), (19.0, 47.0), (19.0, 48.5), (19.0, 50.0), (19.0, 51.5), (19.0, 53.0)]
@@ -26,11 +27,32 @@ def get_adjacent_points(point: tuple[float, float], all_points: list = POINTS):
         if distance <= 4.5: # body jsou od sebe 1.5 vzdalene, takze sousedici body jsou vzdalene max 2*1.5^2=4 od sebe 
             yield point2
 
+# funkce na nalezeni vsech studenych front v datech. fronty se skladaji z bodu, ktere jsou tesne u sebe a maji hodnotu -1
+def find_fronts(row_dict) -> list[list[tuple]]:
+    def find_front_points(point, front_points=[]): # funkce pro rekurzivni hledani front
+        for point2 in get_adjacent_points(point):
+            if point2 in front_points: continue
+            if row_dict[point2] == -1:
+                front_points.append(point2)
+                front_points = find_front_points(point2, front_points=front_points)
+        return front_points
+    fronts = []
+    all_front_points = set() # set vsech bodu co jsou soucasti nejaky fronty
+    for point, value in row_dict.items():
+        if value != -1: continue # zajimaji me jenom studene fronty a ty jsou oznacene hodnotou -1
+        if point in all_front_points: continue
+        front_points = find_front_points(point)
+        all_front_points.update(front_points)
+        if len(front_points) > 2:
+            fronts.append(front_points)
+    return fronts
+
 def main():
-    datetime = 2023081512
+    datetime = 2023081500
     front_df = pl.read_csv('data/fronta.csv')
     row_dict = get_row_dict(datetime, front_df)
-    adjacent_points = list(get_adjacent_points((11.5, 51.5)))
+
+    ic(find_fronts(row_dict))
 
 if __name__ == '__main__':
     main()
